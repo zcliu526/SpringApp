b6c4022ee850f6bc801dc39876a92a374673948e
zcliu526 <Lzc960526>
2022-06-27 18:07:33 -0500

    PostgreSQL added

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,27 @@
 package com.example.demo.student;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Immutable;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
+
     private Long id;
     private String name;
     private Integer age;
